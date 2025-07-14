07f1ac1bf203b92fffade799f5622c8a93b6db06
Aklile Ansa <aklilemente72@gmail.com>
2025-07-15 01:20:00 +0300

    editing the function

--- backend/internal/graphql/client.go
+++ backend/internal/graphql/client.go
@@ -1,16 +1,11 @@
 package graphql
 
 import (
-	
-
-	"github.com/aklile/recipe-backend/internal/config"
-	hasura "github.com/machinebox/graphql"
+	"github.com/machinebox/graphql"
 )
 
+var Client *graphql.Client
 
-var Client *hasura.Client
-var endPOINT = config.ENDPoint()
-func InitClient(){
-	endPoint:= endPOINT
-	Client= hasura.NewClient(string(endPoint))
+func InitClient(endpoint string) {
+	Client = graphql.NewClient(endpoint)
 }
