96c336eacf957b6b7167f1386ce119e221a67964
Aklile Ansa <aklilemente72@gmail.com>
2025-07-14 19:23:13 +0300

    created the user struct

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,1 +1,9 @@
 package models
+
+
+type User struct {
+    ID       string `json:"id"`
+    Email    string `json:"email"`
+    Name     string `json:"name"`
+    Password string `json:"password"`
+}
