d3a35a49bb9217dac9245fe0193fd2933449320a
Aklile Ansa <aklilemente72@gmail.com>
2025-07-14 23:51:01 +0300

    created the insertuser method

--- backend/internal/graphql/mutations.go
+++ backend/internal/graphql/mutations.go
@@ -1,1 +1,44 @@
 package graphql
+
+import (
+	"context"
+	"fmt"
+	
+
+	"github.com/aklile/recipe-backend/internal/config"
+	
+	"github.com/aklile/recipe-backend/internal/models"
+	hasura "github.com/machinebox/graphql"
+)
+var adminSecret = config.LoadADMINSecret()
+func InsertUser(name, email, password string) (models.User, error) {
+	req := hasura.NewRequest(`
+		mutation($name:string!,$email:string!,$password:string!){
+			insert_user_one(object:{
+			name:$name,
+			email:$email,
+			password:$password
+			}){
+			id
+			email
+			name
+			}
+		}
+	`)
+	req.Var("name",name)
+	req.Var("email",email)
+	req.Var("password",password)
+	req.Header.Set("x-hasura-admin-secret",adminSecret)
+
+	var resp struct{
+		InsertUser models.User  `json:"insert_user_one"`
+
+	}
+	err:= Client.Run(context.Background(),req,&resp)
+	if err != nil {
+        return models.User{}, fmt.Errorf("insert user failed: %w", err)
+    }
+
+    return resp.InsertUser, nil
+
+}
