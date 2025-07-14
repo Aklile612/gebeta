14cbf0a9b3c5976e64caa2d1dead191ae08a8aaa
Aklile Ansa <aklilemente72@gmail.com>
2025-07-14 23:50:02 +0300

    added client route

--- backend/internal/graphql/client.go
+++ backend/internal/graphql/client.go
@@ -1,1 +1,16 @@
 package graphql
+
+import (
+	
+
+	"github.com/aklile/recipe-backend/internal/config"
+	hasura "github.com/machinebox/graphql"
+)
+
+
+var Client *hasura.Client
+var endPOINT = config.ENDPoint()
+func InitClient(){
+	endPoint:= endPOINT
+	Client= hasura.NewClient(string(endPoint))
+}
