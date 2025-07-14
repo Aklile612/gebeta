2555291d23acaded366e878578f346cc5d3d6cf8
Aklile Ansa <aklilemente72@gmail.com>
2025-07-14 19:19:10 +0300

    created the login function

--- backend/internal/handlers/login.go
+++ backend/internal/handlers/login.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/aklile/recipe-backend/internal/graphql"
+)
+
+
+type LoginRequest struct{
+	Email string  `json:"emial"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct{
+
+	Token  string `json:"token"`
+	Error string `json:"error,omitempty"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request){
+	var req LoginRequest
+	err:= json.NewDecoder(r.Body).Decode(&req)
+
+	if err!= nil{
+		http.Error(w,"Bad request",http.StatusBadRequest)
+		return
+	}
+	user,err:= graphql
+
+}
