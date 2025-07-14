a6c666c007d17d39a1475fef6b2dcf368f7dde4f
Aklile Ansa <aklilemente72@gmail.com>
2025-07-14 18:05:21 +0300

    created the env loader function

--- backend/internal/config/env.go
+++ backend/internal/config/env.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+func LoadEnv(){
+	err:= godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("NO env file found or error loading it",err)
+	}
+}
+
+func LoadJWTSecret()[]byte{
+	LoadEnv()
+
+	return []byte(os.Getenv("HASURA_GRAPHQL_JWT_SECRET"))
+}
