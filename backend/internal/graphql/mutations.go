75d0ee9f6cca92162af13d9bc2bf0cbaad2e24fd
Aklile Ansa <aklilemente72@gmail.com>
2025-07-15 17:03:42 +0300

    added the log lines

--- backend/internal/graphql/mutations.go
+++ backend/internal/graphql/mutations.go
@@ -1,47 +1,52 @@
-package graphql
+	package graphql
 
-import (
-	"context"
-	"fmt"
-	
+	import (
+		"context"
+		"fmt"
+		
 
-	"github.com/aklile/recipe-backend/internal/config"
-	
-	"github.com/aklile/recipe-backend/internal/models"
-	hasura "github.com/machinebox/graphql"
-)
-var adminSecret = config.LoadADMINSecret()
-func InsertUser(name, email, password string) (models.User, error) {
-	if Client == nil {
-        return models.User{}, fmt.Errorf("graphql client not initialized")
-    }
-	req := hasura.NewRequest(`
-		mutation($name:string!,$email:string!,$password:string!){
-			insert_user_one(object:{
-			name:$name,
-			email:$email,
-			password:$password
-			}){
-			id
-			email
-			name
-			}
+		"github.com/aklile/recipe-backend/internal/config"
+		
+		"github.com/aklile/recipe-backend/internal/models"
+		hasura "github.com/machinebox/graphql"
+	)
+	func InsertUser(name, email, password string) (models.User, error) {
+		fmt.Printf("InsertUser called. Client: %v\n", Client)
+		
+		if Client == nil {
+			return models.User{}, fmt.Errorf("graphql client not initialized")
 		}
-	`)
-	req.Var("name",name)
-	req.Var("email",email)
-	req.Var("password",password)
-	req.Header.Set("x-hasura-admin-secret",string(adminSecret))
+		var adminSecret = config.LoadADMINSecret()
+		fmt.Printf("Client is nil? %v\n", Client == nil)
+		fmt.Printf("Admin Secret: %s\n", string(adminSecret))
 
-	var resp struct{
-		InsertUser models.User  `json:"insert_user_one"`
+		req := hasura.NewRequest(`
+			mutation($name:string!,$email:string!,$password:string!){
+				insert_user_one(object:{
+				name:$name,
+				email:$email,
+				password:$password
+				}){
+				id
+				email
+				name
+				}
+			}
+		`)
+		req.Var("name",name)
+		req.Var("email",email)
+		req.Var("password",password)
+		req.Header.Set("x-hasura-admin-secret",string(adminSecret))
+
+		var resp struct{
+			InsertUser models.User  `json:"insert_user_one"`
+
+		}
+		err:= Client.Run(context.Background(),req,&resp)
+		if err != nil {
+			return models.User{}, fmt.Errorf("insert user failed: %w", err)
+		}
+
+		return resp.InsertUser, nil
 
 	}
-	err:= Client.Run(context.Background(),req,&resp)
-	if err != nil {
-        return models.User{}, fmt.Errorf("insert user failed: %w", err)
-    }
-
-    return resp.InsertUser, nil
-
-}
