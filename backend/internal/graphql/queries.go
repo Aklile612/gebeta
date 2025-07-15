c9bc546cb15c24ed1d16526fa99c70d8d827b4a4
Aklile Ansa <aklilemente72@gmail.com>
2025-07-15 21:40:23 +0300

    added the query finder function

--- backend/internal/graphql/queries.go
+++ backend/internal/graphql/queries.go
@@ -1,1 +1,48 @@
 package graphql
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aklile/recipe-backend/internal/config"
+	"github.com/aklile/recipe-backend/internal/models"
+	hasura "github.com/machinebox/graphql"
+)
+
+
+func GetUserByEmail(email string)(models.User,error){
+	if Client == nil{
+		return models.User{}, fmt.Errorf("GraphQL client not initialized")
+	}
+	adminSecret:= config.LoadADMINSecret()
+
+	req:= hasura.NewRequest(`
+		query($email:String!){
+			users(where:{email:{_eq:$email}}){
+				id
+				email
+				full_name
+				password
+			}
+		}
+	`)
+	req.Var("email",email)
+	req.Header.Set("x-hasura-admin-secret",string(adminSecret))
+
+	var resp struct{
+		Users  []models.User   `json:"users"`
+	}
+
+	err := Client.Run(context.Background(), req, &resp)
+
+	if err != nil {
+		fmt.Printf("❌ InsertUser GraphQL error: %v\n", err)
+		return models.User{}, fmt.Errorf("insert user failed: %w", err)
+	}
+
+	if len(resp.Users)==0{
+		return  models.User{}, fmt.Errorf("user not found")
+	}
+
+	return resp.Users[0],nil
+}
