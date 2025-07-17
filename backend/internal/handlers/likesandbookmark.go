2b43a47b0bbf14e0e8d9ce5908b102dc31253fcf
Aklile Ansa <aklilemente72@gmail.com>
2025-07-17 14:32:04 +0300

    finished creating bookmark handler

--- backend/internal/handlers/likesandbookmark.go
+++ backend/internal/handlers/likesandbookmark.go
@@ -35,5 +35,27 @@ func AddRecipeLikes(c *gin.Context){
 
 
 func ADDRecipeBookmarks(c *gin.Context){
-	
+	recipeID:= c.Param("id")
+
+	userIDInterf,exists:= c.Get("user_id")
+
+	if !exists{
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user not found"})
+		return
+	}
+
+	userID ,ok := userIDInterf.(string)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user id not valied"})
+		return
+	}
+
+	err:= graphql.InsertUserRecipeBookmark(userID,recipeID)
+	if err!= nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":"cannot like the recipe","detail":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{"message":"the recipe liked succefully"})
+
 }
