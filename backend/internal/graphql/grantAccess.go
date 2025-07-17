3027df89d0bca36e7ff0ad67f4dd141b794fb821
Aklile Ansa <aklilemente72@gmail.com>
2025-07-18 02:50:40 +0300

    fixed some typos

--- backend/internal/graphql/grantAccess.go
+++ backend/internal/graphql/grantAccess.go
@@ -14,13 +14,13 @@ func GrantAccess(userID, recipeID string) error{
 	adminSecret:= config.LoadADMINSecret()
 
 	req:= hasura.NewRequest(`
-		mutation ($user_id: uuid!, $recipe_id: uuid!, $granted_at: timestamptz!){
+		mutation ($user_id: uuid!, $recipe_id: uuid!, $granted_at: timestamp!){
 			insert_recipe_access_one(object:{
 				user_id: $user_id,
 				recipe_id: $recipe_id,
-				granted_at: $granted_at,
+				access_granted_at: $granted_at,
 			},on_conflict: {
-				contraint: recipe_access_user_id_recipe_id_key,
+				constraint: recipe_access_user_id_recipe_id_key,
 				update_columns: [access_granted_at]  
 			}){
 				id	
@@ -30,7 +30,7 @@ func GrantAccess(userID, recipeID string) error{
 
 	req.Var("user_id",userID)
 	req.Var("recipe_id",recipeID)
-	req.Var("access_granted_at",time.Now().UTC().Format(time.RFC3339))
+	req.Var("granted_at",time.Now().Format("2006-01-02 15:04:05"))
 
 	req.Header.Set("x-hasura-admin-secret", string(adminSecret))
 
