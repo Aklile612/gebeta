f78d0b8efd0fc9fedd700747bb2bca9dd5ec3509
Aklile Ansa <aklilemente72@gmail.com>
2025-07-17 18:04:06 +0300

    added the delete mutation graph

--- backend/internal/graphql/mutations.go
+++ backend/internal/graphql/mutations.go
@@ -280,3 +280,32 @@
 
 		return Client.Run(context.Background(),req,&resp)
 	}
+
+	func DeleteRecipeLike(userID , recipeID string) error{
+
+		adminSecret:= config.LoadADMINSecret()
+
+		req:= hasura.NewRequest(`
+			mutation($user_id: uuid! , $recipe_id: uuid!){
+				delete_recipe_likes(where: {
+					user_id: {_eq: $user_id},
+					recipe_id: {_eq: $recipe_id}
+				}){
+					affected_rows	
+				}
+			}
+		`)
+
+		req.Var("user_id",userID)
+		req.Var("recipe_id",recipeID)
+		req.Header.Set("x-hasura-admin-secret", string(adminSecret))
+
+		var resp struct{
+
+			DeleteRecipeLikes struct{
+				AffectedRows int `json:"affected_rows"`
+			}`json:"delete_recipe_likes"`
+		}
+
+		return Client.Run(context.Background(),req,&resp)
+	}
