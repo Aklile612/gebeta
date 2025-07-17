317d80738c8fca4b0c1d97754af576f7971495e4
Aklile Ansa <aklilemente72@gmail.com>
2025-07-17 13:55:36 +0300

    writing the function

--- backend/internal/handlers/likesandbookmark.go
+++ backend/internal/handlers/likesandbookmark.go
@@ -1,1 +1,26 @@
-func AddRecipeLikes()
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddRecipeLikes(c *gin.Context){
+	recipeID := c.Param("id")
+
+	userIDInterf, exists := c.Get("user_id")
+
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user not found"})
+		return
+	}
+	userID, ok := userIDInterf.(string)
+
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user id not valied"})
+		return
+	}
+	
+
+}
