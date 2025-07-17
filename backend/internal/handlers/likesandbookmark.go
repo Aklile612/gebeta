26a08a3015bd20af7a0a50246e65ea115acc4d62
Aklile Ansa <aklilemente72@gmail.com>
2025-07-17 15:17:24 +0300

    finished creating rating handler

--- backend/internal/handlers/likesandbookmark.go
+++ backend/internal/handlers/likesandbookmark.go
@@ -61,5 +61,36 @@ func ADDRecipeBookmarks(c *gin.Context){
 }
 
 func AddRatingtoRecipe(c *gin.Context){
-	
+	recipeID := c.Param("id")
+
+	userIDInterf, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user not found"})
+		return
+	}
+	userID, ok := userIDInterf.(string)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid user ID"})
+		return
+	}
+
+	var body struct{
+		Rating int `json:"rating"`
+	}
+
+	err:= c.BindJSON(&body)
+
+	if err != nil || body.Rating < 1 || body.Rating > 5 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "rating must be between 1 and 5"})
+		return
+	}
+
+	err = graphql.InsertRatingByUser(userID,recipeID,body.Rating)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not rate recipe"})
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{"message":"recpe succesfully rated"})
 }
