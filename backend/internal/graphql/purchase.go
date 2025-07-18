57a655bca20ac933b7de15af0ae533adc1ce5983
Aklile Ansa <aklilemente72@gmail.com>
2025-07-18 20:27:31 +0300

    added a the query to check if userhas access

--- backend/internal/graphql/purchase.go
+++ backend/internal/graphql/purchase.go
@@ -97,6 +97,37 @@ func GetRecipePurchaseInfo(recipeID,requesterUserID string) (*RecipePurchaseInfo
 }
 
 func UserHasRecipeAccess(userID,recipeID string) (bool,error){
+	adminSecret := config.LoadADMINSecret()
 
-	
+	req:= hasura.NewRequest(`
+		query ($user_id: uuid!, $recipe_id: uuid!) {
+  			recipe_access_aggregate(
+  			  where: { user_id: { _eq: $user_id }, recipe_id: { _eq: $recipe_id } }
+  			) {
+  			  aggregate {
+  			    count
+  			  }
+  			}
+		}
+	`)
+
+	req.Var("user_id", userID)
+	req.Var("recipe_id", recipeID)
+	req.Header.Set("x-hasura-admin-secret", string(adminSecret))
+
+	var resp struct {
+		RecipeAccessAggregate struct {
+			Aggregate struct {
+				Count int `json:"count"`
+			} `json:"aggregate"`
+		} `json:"recipe_access_aggregate"`
+	}
+	err := Client.Run(context.Background(), req, &resp)
+
+	if err!= nil{
+		return false,err
+	}
+
+	return resp.RecipeAccessAggregate.Aggregate.Count > 0, nil
+
 }
