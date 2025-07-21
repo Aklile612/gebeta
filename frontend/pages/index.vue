942d2cd49dadaae5e256a5f014de509456b96ebc
Aklile Ansa <aklilemente72@gmail.com>
2025-07-21 20:47:01 +0300

    edited

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,6 +3,9 @@
       <h1 class="text-sm bg-slate-400 ">Home Page</h1>
       <!-- form or content -->
       <button class="btn btn-primary">Click Me</button>
+      <!-- <Utensils color=""/> -->
+      <h2 class="mt-20 bg-red-200 text-black">Hi</h2>
+      <div>wassup</div>
     </div>
 
   </template>
