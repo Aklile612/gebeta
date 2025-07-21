366d48bd0883e5167392cf8e9d4ae79c6ca0ca61
Aklile Ansa <aklilemente72@gmail.com>
2025-07-21 19:46:54 +0300

    created index page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h1 class="text-sm bg-slate-400 ">Home Page</h1>
+      <!-- form or content -->
+      <button class="btn btn-primary">Click Me</button>
+    </div>
+
+  </template>
+  
