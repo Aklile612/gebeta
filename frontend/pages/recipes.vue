503ffe1476f4fac44bcc0e389d10653a9a246a0d
Aklile Ansa <aklilemente72@gmail.com>
2025-07-24 02:43:46 +0300

    Add latest frontend changes

--- frontend/pages/recipes.vue
+++ frontend/pages/recipes.vue
@@ -1,17 +1,26 @@
 <script setup>
-
+import prepTime from '~/components/prepTime.vue';
+import search from '~/components/search.vue';
 </script>
 
 <template>
     <div class="bg-white">
         <!-- title -->
-        <div></div>
+        <div class="h-[20vh]">
+            <p class="text-xl font-bold">All Recipes</p>
+            <p class="text-sm">Browes meals shared by the users around the world</p>
+        </div>
 
 
 
         <!-- main recipe part -->
-         <div>
-            
+         <div class="bg-[#c7c2c2] w-full h-[80vh]">
+            <div>
+                <Search/>
+            </div>
+            <div>
+                <prepTime/>
+            </div>
          </div>
     </div>
 
