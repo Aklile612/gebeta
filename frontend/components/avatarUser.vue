b4ba7b462e039f335a62fef11cc2a241dabeda87
Aklile Ansa <aklilemente72@gmail.com>
2025-07-23 18:02:26 +0300

    Add latest frontend changes

--- frontend/components/avatarUser.vue
+++ frontend/components/avatarUser.vue
@@ -4,12 +4,12 @@
 
 
 <template>
-    <div class="bg-white  w-full h-[97vh]">
+    <div class="bg-white  w-full h-[60vh]">
         <div class="mx-[30vw] pt-[40px]">
             <h1 class="mb-5 text-4xl ml-[7vw] font-serif  font-bold">Top Recipe Creators</h1>
 
         </div>
-        <div class="w-[100px] h-[70px] bg-[#fae3cd] rounded-[5px]">
+        <div class="w-[150px] h-[100px] bg-[#fae3cd] rounded-[5px]">
             <div class="avatar">
               <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring-2 ring-offset-2">
                 <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
@@ -17,6 +17,8 @@
             </div>
             <div class="mt-2 ">
                 <p class="font-bold text-sm">Alana Germy</p>
+
+                <button class="btn btn-soft btn-warning hover:bg-orange-500">View Recipes</button>
             </div>
         </div>
 
