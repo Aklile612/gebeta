bb7d33ac91f3ee7c381a1631bec824728279ab36
Aklile Ansa <aklilemente72@gmail.com>
2025-07-23 20:14:29 +0300

    Add latest frontend changes

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-      <h1>Login Page</h1>
-      <!-- form or content -->
+  <div class="flex min-h-screen">
+    <!-- Hero Image Side -->
+    <div
+      class="hero w-1/2 min-h-screen"
+      style="background-image: url('/image/login.png');"
+    >
+      <div class="hero-overlay bg-opacity-10"></div>
+      <div class="hero-content text-neutral-content text-center">
+        <div class="max-w-md">
+          
+          <p class="mt-7">Discover Amazing Recipes</p>
+          <p class="mt-7">Join thousends of food lover sharing there favorite recipes</p>
+        </div>
+      </div>
     </div>
+  
+    <!-- Optional Other Side -->
+    <div class="w-1/2 bg-white flex items-center justify-center">
+      <p class="text-gray-800">the other side</p>
+    </div>
+  </div>
+  
   </template>
   
