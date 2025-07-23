e1b49e332e66d4a955f40eb0abb08ee76d98fb9f
Aklile Ansa <aklilemente72@gmail.com>
2025-07-23 18:05:08 +0300

    Add latest frontend changes

--- frontend/components/avatarUser.vue
+++ frontend/components/avatarUser.vue
@@ -9,9 +9,9 @@
             <h1 class="mb-5 text-4xl ml-[7vw] font-serif  font-bold">Top Recipe Creators</h1>
 
         </div>
-        <div class="w-[150px] h-[100px] bg-[#fae3cd] rounded-[5px]">
+        <div class="w-[300px] h-[200px] bg-[#fae3cd] rounded-[5px]">
             <div class="avatar">
-              <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring-2 ring-offset-2">
+              <div class="ring-primary ring-offset-base-100 w-15 rounded-full ring-2 ring-offset-2">
                 <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
               </div>
             </div>
