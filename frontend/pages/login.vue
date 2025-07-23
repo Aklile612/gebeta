6cbf9f9f80856a06bbcc642f61f01e205b92e613
Aklile Ansa <aklilemente72@gmail.com>
2025-07-23 23:35:55 +0300

    Add latest frontend changes

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,23 +1,36 @@
+<script setup>
+  definePageMeta({
+    layout:false
+  })
+</script>
+
+
 <template>
   <div class="flex min-h-screen">
-    <!-- Hero Image Side -->
-    <div
-      class="hero w-1/2 min-h-screen"
-      style="background-image: url('/image/login.png');"
-    >
-      <div class="hero-overlay bg-opacity-10"></div>
-      <div class="hero-content text-neutral-content text-center">
+    <!-- image background -->
+    <div class="hero w-1/2 min-h-screen bg-no-repeat bg-center bg-cover" style="background-image: url('/image/login.png');">
+      <div class="flex flex-col justify-end items-center text-neutral-content w-full h-full pb-10 text-center">
         <div class="max-w-md">
-          
           <p class="mt-7">Discover Amazing Recipes</p>
-          <p class="mt-7">Join thousends of food lover sharing there favorite recipes</p>
+          <p class="mt-7">Join thousands of food lovers sharing their favorite recipes</p>
         </div>
       </div>
     </div>
+
   
     <!-- Optional Other Side -->
     <div class="w-1/2 bg-white flex items-center justify-center">
-      <p class="text-gray-800">the other side</p>
+      <!-- top part icon and shit -->
+       <div>
+        <div class="w-16 h-16 bg-orange-500 rounded-full flex items-center justify-center shadow-md">
+                  <IconUtensils class="text-white w-8 h-8" />
+        </div>
+
+       </div>
+       <!-- welcome and login form -->
+        <div></div>
+
+
     </div>
   </div>
   
